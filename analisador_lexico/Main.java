066ff8b22b4a57cfb03faa0d136fab1c5d5cf17d
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-03-23 18:28:56 -0300

    Adiciona arquivo para teste da leitura de arquivos e altera arquivo main pra ler o arquivo

--- analisador_lexico/Main.java
+++ analisador_lexico/Main.java
@@ -1,5 +1,10 @@
 package projeto.analisador_lexico;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -9,6 +14,12 @@ public class Main {
         // String data = "float x = 1.1\n int y = 5\nfloat z = 2.2\nint w = 10\nint
         // result = x * y + z - w\nresult = result * 2\nresult = result / 2\nreturn 4";
         String data = "gaviao x = 1.1\n taOk y = 5\n gaviao z = 2.2\n taOk= 10\n taOk result = x * y + z";
+
+        ArrayList<String> operacoes = new Main().Arquivo();
+        for (String operacao : operacoes) {
+            System.out.println(operacao);
+        }
+
         Lexer lexer = new Lexer(data);
         tokens = lexer.getTokens();
 
@@ -16,4 +27,22 @@ public class Main {
             System.out.println(token);
         }
     }
+
+    public ArrayList<String> Arquivo() {
+        ArrayList<String> operacoes = new ArrayList<>();
+        try {
+            FileReader arquivo = new FileReader("teste.txt");
+            BufferedReader br = new BufferedReader(arquivo);
+
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                operacoes.add(linha);
+            }
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return operacoes;
+    }
+
 }

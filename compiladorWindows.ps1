5cf589ededdee18f995e25ea53aa6164724e2add
AlvarocJesus <58888859+AlvarocJesus@users.noreply.github.com>
2024-05-21 00:53:59 -0300

    Adiciona funcao de input

--- compiladorWindows.ps1
+++ compiladorWindows.ps1
@@ -1,26 +1,20 @@
 cls
 
-# echo "Iniciando Compilador Unix"
-
-# javac */*.java
-
-# java ./projeto/Main >> Main.java
-
-# javac Main.java
-
-# java Main
-
 # Escreva a mensagem inicial
 Write-Host "Iniciando Compilador PowerShell"
 
+Remove-Item .\*\*.class -ErrorAction SilentlyContinue
+
 # Compile todos os arquivos Java na pasta atual e subpastas
-Get-ChildItem -Recurse -Filter "*.java" | ForEach-Object {javac $_.FullName}
+javac .\Lexico\*.java
+javac .\Sintatico\*.java
+javac .\projeto\*.java
 
 # Execute o arquivo Java e redirecione a saída para um novo arquivo Java na pasta raiz
-java .\projeto\Main.java > .\Main.java
+java .\projeto\Main.java > Main.java
 
 # Compile o novo arquivo Java
-javac Main.java
+# javac Main.java
 
 # Execute o novo arquivo Java
-java Main
+# java -cp "$(pwd | Select-String [A-Z].*)" Main
